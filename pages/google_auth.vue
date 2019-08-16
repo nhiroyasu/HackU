f7d1e2c006ecd3cfea1a58ffe7324bb5e7acb4a2
Namid_M <hiroyasu11099011@gmail.com>
2019-08-17 00:23:03 +0900

    add pages About auth

---
+++ pages/google_auth.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <div>
+      <div class="auth-detail">
+        <h1>{{title}}</h1>
+        <p>{{message}}</p>
+      </div>
+      <p>{{ $store.state.user }}</p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.auth-detail {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
+  min-width: 300px;
+  min-height: 500px;
+  background: #dfe6e9;
+  color: #636e72;
+}
+</style>
+
+
+<script>
+import firebase from '~/plugins/firebase.js';
+
+export default {
+  components: {
+    firebase,
+  },
+  data: function() {
+    return {
+      title: 'Auth',
+      message: 'this is message.',
+    };
+  },
+  created: function() {
+    firebase.login_g_auth(this.$store);
+  }
+}
+</script>
